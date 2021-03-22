e817f927962fb5bc9345f15fc2e614ee0e1ce41c
Jonas <jonas_elmesten@hotmail.com>
2021-03-22 17:56:41 +0100

    Added functionality to verify whole blockchain.

--- Blockchain.py
+++ Blockchain.py
@@ -1,4 +1,5 @@
 import json
+import random
 import time
 from hashlib import sha256
 
@@ -6,6 +7,7 @@ from Block import Block
 
 
 class Blockchain:
+
     difficulty = 5
 
     def __init__(self):
@@ -15,19 +17,16 @@ class Blockchain:
         self.create_genesis_block()
 
     def create_genesis_block(self):
-
         genesis_block = Block([], "0")
         self.chain.append(genesis_block)
 
     def add_block(self, block):
-
-        previous_block = self.get_last_block()
         self.chain.append(block)
 
     def get_last_block(self):
         return self.chain[-1]
 
-    def is_chain_valid(self):
+    def is_chain_valid(self, print_block_hash=False):
 
         # Index to go through 2 succeeding blocks
         block_indexes = zip(range(len(self.chain) - 1, -1, -1),
@@ -38,13 +37,24 @@ class Blockchain:
 
             current_block = self.chain[current_block_index]
             previous_block = self.chain[previous_block_index]
-            
-            print("CURRENT:", current_block.previous_hash)
-            print("PREVIOUS:", previous_block.compute_hash())
-            print("--------")
 
-            if current_block.previous_hash != previous_block.compute_hash():
+            current_hash = current_block.previous_hash
+            previous_hash = previous_block.compute_hash()
+
+            if print_block_hash:
+                print("CURRENT:", current_hash)
+                print("PREVIOUS:", previous_hash)
+
+            if current_hash != previous_hash:
                 return False
 
-        print("Chain is VALID!")
+        print("Chain is VALID! Size:", len(self.chain), "Block data:", self.get_last_block().data)
         return True
+
+    def start_blockchain(self):
+
+        while True:
+            block = Block(str(random.randint(1,10000)), self.get_last_block().compute_hash())
+            block.mine_block()
+            self.add_block(block)
+            self.is_chain_valid(print_block_hash=True)
--- starter.py
+++ starter.py
@@ -3,15 +3,6 @@ from Blockchain import Blockchain
 
 blockchain = Blockchain()
 
-def add_block():
-    block = Block("fetto", blockchain.get_last_block().compute_hash())
-    block.mine_block()
-    blockchain.add_block(block)
-
-add_block()
-add_block()
-add_block()
-blockchain.is_chain_valid()
-
+blockchain.start_blockchain()
 
 
